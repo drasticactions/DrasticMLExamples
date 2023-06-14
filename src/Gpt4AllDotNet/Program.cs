c84dd1d2eec03aaca65f1943a3a8ac87d9a481d1
Tim Miller <innerlogic4321@gmail.com>
2023-06-14 16:20:06 +0900

    Start apps

---
+++ src/Gpt4AllDotNet/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+using Gpt4All;
+using Sharprompt;
+using Console = System.Console;
+
+internal class Program
+{
+    internal static MainProgram? program;
+
+    private static async Task<int> Main(string[] args)
+    {
+        program = new MainProgram(args);
+        return await program.RunAsync();
+    }
+}
+
+internal class MainProgram
+{
+    private RootCommand root;
+    private readonly string[] args;
+
+    public MainProgram(string[] args)
+    {
+        this.args = args;
+        var localModelOption = new Option<string>("--model", "GPT4All Model");
+        var promptOption = new Option<string>("--prompt", "Set prompt");
+
+        var promptCommand = new Command("prompt")
+        {
+            Handler = CommandHandler.Create(LocalFiles),
+        };
+
+        promptCommand.AddOption(localModelOption);
+        promptCommand.AddOption(promptOption);
+
+        this.root = new RootCommand
+        {
+            promptCommand,
+        };
+    }
+
+    private async Task LocalFiles(string model, string prompt)
+    {
+        model = this.GetModelPrompt(model);
+        if (!File.Exists(model))
+        {
+            return;
+        }
+
+        prompt ??= Prompt.Input<string>("Enter prompt");
+
+        var modelFactory = new Gpt4AllModelFactory();
+        var gpt4All = modelFactory.LoadModel(model);
+        var result = await gpt4All.GetStreamingPredictionAsync(
+            prompt,
+            PredictRequestOptions.Defaults);
+        Console.WriteLine(Environment.NewLine);
+        await foreach (var token in result.GetPredictionStreamingAsync())
+        {
+            Console.Write(token);
+        }
+    }
+
+    private string GetModelPrompt(string? modelPath = "")
+    {
+        if (File.Exists(modelPath))
+        {
+            return modelPath;
+        }
+
+        return Prompt.Input<string>("Enter model path.");
+    }
+
+
+    public async Task<int> RunAsync()
+    {
+        return await this.root.InvokeAsync(this.args);
+    }
+}
---
+++ src/WhisperDotNet/Program.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="Program.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+using Sharprompt;
+
+internal class Program
+{
+    internal static MainProgram? program;
+
+    private static async Task<int> Main(string[] args)
+    {
+        program = new MainProgram(args);
+        return await program.RunAsync();
+    }
+}
+
+internal class MainProgram
+{
+    private RootCommand root;
+    private readonly string[] args;
+
+    public MainProgram(string[] args)
+    {
+        this.args = args;
+        var localModelOption = new Option<string>("--model", "Whisper Model");
+
+        var promptCommand = new Command("transcribe")
+        {
+            Handler = CommandHandler.Create(Transcribe),
+        };
+
+        promptCommand.AddOption(localModelOption);
+
+        this.root = new RootCommand
+        {
+            promptCommand,
+        };
+    }
+
+    public async Task<int> RunAsync()
+    {
+        return await this.root.InvokeAsync(this.args);
+    }
+
+    private async Task Transcribe(string model)
+    {
+        model = this.GetModelPrompt(model);
+        if (!File.Exists(model))
+        {
+            return;
+        }
+    }
+
+    private string GetModelPrompt(string? modelPath = "")
+    {
+        if (File.Exists(modelPath))
+        {
+            return modelPath;
+        }
+
+        return Prompt.Input<string>("Enter model path.");
+    }
+}
